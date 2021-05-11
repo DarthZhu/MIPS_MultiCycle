232eb94364c1d510e66f1f8194c086628560cbd0
DarthZhu <1821357816@qq.com>
2021-05-11 16:44:32 +0800

    Register add en signal

--- sources_1/imports/new/mipspart.sv
+++ sources_1/imports/new/mipspart.sv
@@ -20,15 +20,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module flopr #(parameter WIDTH = 8)(
-    input logic clk, reset,
-    input logic [WIDTH-1 : 0] d,
-    output logic [WIDTH-1 : 0] q
+module floprenr #(parameter WIDTH = 8)(
+    input  logic                clk, reset,
+    input  logic                en,
+    input  logic [WIDTH-1 : 0]  d,
+    output logic [WIDTH-1 : 0]  q
 );
     always_ff @(posedge clk, posedge reset)
     begin
-        if (reset) q <= 0;
-        else q <= d;    
+        if (reset)   q <= 0;
+        else if (en) q <= d;
     end
 endmodule
 
